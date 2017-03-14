cffd0767f670c73c3f6a4a5a9e26fdd3402e20be
kpietru <kpietru@users.noreply.github.com>
2017-03-14 15:34:32 +0100

    Update Matcher.js

--- Matcher/Matcher.js
+++ Matcher/Matcher.js
@@ -25,7 +25,7 @@ module.exports = class Matcher
      *
      * @param Routes routes
      * @param string requestMethod
-     * @param string requestUrl
+     * @param string requestUrl    Only path or full url.
      */
     constructor(routes, requestMethod, requestUrl)
     {
